f8a9bd57b93d7e633b5e67f48c4a7497766a96ba
unknown <knosmos@gmail.com>
2021-12-13 20:36:50 -0500

    Better solution for Day 13

--- 13/day13-1.py
+++ 13/day13-1.py
@@ -4,16 +4,14 @@ for c, i in enumerate(data):
     if len(i) == 0:
         break
     points.append(list(map(int,i.split(","))))
-instruction = data[c+1][11:]
-dir = instruction[0]
-val = int(instruction[2:])
-
-m = [[0]*2000 for _ in range(2000)]
-for i in points:
-    m[i[0]][i[1]] = 1
+c += 1
+k = data[c][11:]
+dir = k[0]
+val = int(k[2:])
 if dir == "x":
-    for i in range(0,2000):
-        if i < val:
-            m[i] = [min(1,m[i][j]+m[val-i+val][j]) for j in range(2000)]
-    m = m[:val]
-print(sum(sum(i) for i in m))
+    for i in points:
+        if i[0] > val: i[0] = val-(i[0]-val)
+elif dir == "y":
+    for i in points:
+        if i[1] > val: i[1] = val-(i[1]-val)
+print(len(set(map(tuple,points))))
--- 13/day13-2.py
+++ 13/day13-2.py
@@ -4,28 +4,22 @@ for c, i in enumerate(data):
     if len(i) == 0:
         break
     points.append(list(map(int,i.split(","))))
-
-m = [[0]*2000 for _ in range(2000)]
-for i in points:
-    m[i[0]][i[1]] = 1
 for i in range(12):
     c += 1
-    instruction = data[c][11:]
-    dir = instruction[0]
-    val = int(instruction[2:])
+    k = data[c][11:]
+    dir = k[0]
+    val = int(k[2:])
     if dir == "x":
-        for i in range(0,len(m)):
-            if i < val:
-                m[i] = [min(1,m[i][j]+m[val-i+val][j]) for j in range(len(m[i]))]
-        m = m[:val]
+        for i in points:
+            if i[0] > val: i[0] = val-(i[0]-val)
     elif dir == "y":
-        for i in range(0,len(m)):
-            l = m[i][:val]
-            r = m[i][val:][::-1]
-            r = [0]*(len(l)-len(r)) + r
-            m[i] = [min(1,l[j]+r[j]) for j in range(len(l))]
-for i in range(len(m[0])):
-    for j in range(len(m)):
+        for i in points:
+            if i[1] > val: i[1] = val-(i[1]-val)
+m = [[0]*6 for _ in range(40)]
+for i in points:
+    m[i[0]][i[1]] = 1
+for i in range(6):
+    for j in range(40):
         if m[j][i] == 1:
             print("#", end="")
         else:
